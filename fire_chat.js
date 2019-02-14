b95206781276a57cff10a95d52404bb1805bcc83
Am-Coder <mishraaman2210@gmail.com>
2019-02-14 22:49:13 +0530

    V2

--- fire_chat.js
+++ fire_chat.js
@@ -1,32 +1,47 @@
 var config = {
-    apiKey: "AIzaSyDvvnPLQMzF3JOdg-ddsdV5sah6FpEpOsY",
-    authDomain: "doctalkfinal-ce5b5.firebaseapp.com",
-    databaseURL: "https://doctalkfinal-ce5b5.firebaseio.com",
-    projectId: "doctalkfinal-ce5b5",
-    storageBucket: "doctalkfinal-ce5b5.appspot.com",
-    messagingSenderId: "1039956260614"
+    apiKey: "AIzaSyDMyu454hQSUzQO5_YynSBr6Ec2lc35Hp4",
+    authDomain: "p-to-p-6d1ab.firebaseapp.com",
+    databaseURL: "https://p-to-p-6d1ab.firebaseio.com",
+    projectId: "p-to-p-6d1ab",
+    storageBucket: "p-to-p-6d1ab.appspot.com",
+    messagingSenderId: "1025663182708"
   };
-  firebase.initializeApp(config);
+  var goToChat;//Declared outside due to scoping issues.
+$(document).ready(
+    function(){
 
-  function goToChat(a){
-      user = firebase.auth().currentUser.uid;
-    if(a==0) {
-        var ref=firebase.database().ref('doctor/' + user);
-        let sd = window.location.href;
-        window.location.href = sd.replace("doctor_home","index");
-    }
-    else {
-        var ref=firebase.database().ref('patient/' + user);
-        let sp = window.location.href;
-        window.location.href = sp.replace("patient_home","index");
-    }
+console.log("adasd");
+// document.getElementById("connectctBtn").onclick() = function(){
+//     var email = $(this).parent().parent().attr('id');
+//     console.log("fsdf"+email);
+//     
+// goToChat(email);
+// }
+// $("#connectBtn").on('click', function(){
+//     console.log("adasd");
+
+//     var email = $(this).parent().parent().attr('id');
+//     console.log("fsdf"+email);
+//     goToChat(email);
+// })
+    goToChat = function() {
+        var joinMail = $(this).parent().parent().attr('id');
+
+        user = firebase.auth().currentUser.uid;
+        
+        var ref=firebase.database().ref('Users/' + user);
+
+    
+
 
     ref.on('value',function (snapshot){
-        console.log(snapshot.val().email);
+        userMail=snapshot.val().email;
+        var joined = userMail + joinMail;
+        firebase.database().ref().child('Chat').child(user).push();
+        let sd = window.location.href;
+        window.location.href = sd.replace("main","index");
     })
 }
 
-function goToDialogflow() {
-    let s = window.location.href;
-    window.location.href = s.replace("patient_home","trydialogflow");
-}
+    
+})
